5b28886618598110d95492e4a7f93671fab24e01
aaeras <mainaaeras@gmail.com>
2025-10-31 00:04:36 +1100

    Really basic stuff, just outlining required functions, need a class obvs, and more research into tabulatio hashing, stash and single vector cuckoo hashing specifics

---
+++ cuckoo-hashing/cuckooTable.cpp
@@ -0,0 +1,21 @@
+#include "cuckooTable.hpp"
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+//https://github.com/microsoft/Kuku/tree/main
+//https://en.wikipedia.org/wiki/Tabulation_hashing
+
+void insert(int item) {
+    std::ignore = item;
+}
+
+bool contains(int item) {
+    return true;
+}
+
+void tabulationHash(int item) {
+    std::ignore = item;
+}
+
+
