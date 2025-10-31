22faf99278029749c393022dcb7ecf9bba9f4fbd
aaeras <mainaaeras@gmail.com>
2025-10-31 21:33:29 +1100

    Added class, and methods for generating tabulation table, need to make table properly, do hash function etc

--- cuckoo-hashing/cuckooTable.cpp
+++ cuckoo-hashing/cuckooTable.cpp
@@ -2,20 +2,37 @@
 #include <vector>
 #include <algorithm>
 #include <tuple>
-
+#include <random>
+//https://stackoverflow.com/questions/56655002/generating-a-random-uint64-t
 //https://github.com/microsoft/Kuku/tree/main
 //https://en.wikipedia.org/wiki/Tabulation_hashing
+class CuckooTable  {
+    private: 
+        uint64_t generateRandomUInt64_T() {
+            std::random_device random;
+            std::mt19937_64 generate(random());
+            std::uniform_int_distribution<uint64_t> distribution;
+            return distribution(generate);
+        };
+        std::vector<std::vector<uint64_t>> populateTable(std::vector<std::vector<std::uint64_t>> table) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 256; j++) {
+                table[i][j] = generateRandomUInt64_T();
+            }
+        }
+        return table;
+        }
 
-void insert(int item) {
-    std::ignore = item;
-}
+    public: 
+        void insert(int item) {
+            std::ignore = item;
+        }
 
-bool contains(int item) {
-    return true;
-}
+        bool contains(int item) {
+            return true;
+        }
 
-void tabulationHash(int item) {
-    std::ignore = item;
-}
-
-
+        void tabulationHash(int item) {
+            std::ignore = item;
+        }
+};
