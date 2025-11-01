dee326aa1bfb5b696e50dab4f0cb93026eb68bdc
aaeras <mainaaeras@gmail.com>
2025-11-01 21:29:58 +1100

    Changes to hash value to supposedly make it indice based, not sure tabulation hashing is black magic

--- cuckoo-hashing/cuckooTable.cpp
+++ cuckoo-hashing/cuckooTable.cpp
@@ -9,6 +9,9 @@
 class CuckooTable  {
     private:
         std::pair<std::uint64_t, std::uint64_t> table[8][256];
+        std::vector<uint64_t> table1;
+        std::vector<uint64_t> table2; //need to assert size at start ?
+        int capacity;
         uint64_t generateRandomUInt64_T() {
             std::random_device random;
             std::mt19937_64 generate(random());
@@ -17,6 +20,7 @@ class CuckooTable  {
         };
     public: 
         CuckooTable() {
+            capacity = 6; //initialize based on this
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 256; j++) {
                     auto first = generateRandomUInt64_T();
@@ -25,29 +29,36 @@ class CuckooTable  {
             }
         }
         }
-        uint64_t firstTabulationHash(uint64_t key) {
+        int getCapacity() {
+            return capacity;
+        }
+        int firstTabulationHash(uint64_t key) {
             uint64_t residual = 0;
             for (int i = 0; i < 8; i++) {
                 residual ^= table[i][(char)(key >> 8*i)].first;
             }
-            return residual;
+            return residual & capacity - 1;
         }
-        uint64_t secondTabulationHash(uint64_t key) {
+        int secondTabulationHash(uint64_t key) {
             uint64_t residual = 0;
             for (int i = 0; i < 8; i++) {
                 residual ^= table[i][(char)(key >> 8*i)].second;
             }
-            return residual;
+            return residual & capacity - 1;
         }
         void insert(int item) {
-            std::ignore = item;
+            //generate locations with first and second hash
+            //search table for item
+            //kick a bunch of times
+            //quit at max kicks
         }
 
         bool contains(int item) {
-            return true;
+            //should be able to hash ? otherwise search whole table
         }
 
         void rehash() {
-            //
+            //recreate whole table, move to 2x size or something
+
         }
 };
