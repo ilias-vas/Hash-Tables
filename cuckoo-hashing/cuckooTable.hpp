fab0ca12e8c643e1accade6940af4c0bf3e31ad2
aaeras <mainaaeras@gmail.com>
2025-11-02 15:08:33 +1100

    Created proper structure for file in header file, need to change relevant functions, constructors etc which will be in next commit, no new content, just moving things around, testing shows hash functions work though :)

--- cuckoo-hashing/cuckooTable.hpp
+++ cuckoo-hashing/cuckooTable.hpp
@@ -3,3 +3,23 @@
 #include <algorithm>
 #include <tuple>
 #include <random>
+class CuckooTable {
+    private:
+        std::vector<uint64_t> table1;
+        std::vector<uint64_t> table2;
+        int capacity;
+        const int maxKicks = 500;
+        uint64_t generateRandomUInt64_T();
+        void initializeTable();
+        bool attemptInsert(uint64_t item);
+    public:
+        CuckooTable(int capacity = 16);
+        void insert(uint64_t item);
+        bool conatins(uint64_t item);
+        bool remove(uint64_t item);
+        uint64_t firstTabulationHash(uint64_t item);
+        uint64_t secondTabulationHash(uint64_t item);
+        void rehash();
+        int getCapacity();
+        int getSize();
+};
